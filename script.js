d7d9bb693d11fe6bee056a82aa1479c975751672
Karthik1462 <139427130+Karthik1462@users.noreply.github.com>
2024-06-12 10:23:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,228 @@
+// //const marks =75;
+
+// //if (marks>=90){
+//  //   console.log("Grade A+")
+// //} else if(marks>=80){
+//   //  console.log("Grade A")
+// //} else if(marks>=70){
+//     //console.log("Grade B")
+// //}else{
+// //    console.log("You Failed")/
+// //}
+
+// //const age = 70
+
+// //if (age>=18){
+//  //   console.log("You are inside if")
+//  //   console.log("You can vote")
+// //
+//   //  if (age>=60){
+//   //      console.log("You can vote twice")
+//  //   }
+// //}else{
+// //    console.log("You are too young to vote")
+// //
+
+// //const marks = 50
+
+// //const result = (marks>=40)? "Passed":"Failed"
+
+// /* const grade = 'A';
+
+// switch(grade){
+//     case "A":
+//         console.log("You did very well")
+//         break;
+
+//     case "B":
+//         console.log("Good, keep learning")
+//         break;
+
+//     case "C":
+//         console.log("You need to improve")
+//         break;
+
+//     default:
+//         console.log("You have failed")
+//         break;
+
+//  }
+
+
+//  for(let index =0; index <5; index++){
+//     console.log("Hi There", index)
+//  }
+
+//  let step = 5;
+//  while(step>0){
+//     console.log(step);
+//     step--;
+//  }
+
+// const words =[
+//     "Apple", 
+//     "Banana",
+//      12, 
+//      true,
+//      {
+//         name : "Karthik"
+//      },
+//      function hello(){
+//          console.log("Hello World")
+//      },
+// ]
+
+// console.log(words.length)
+// const newword =words;
+// newword[0] = "banana";
+// console.log(words.indexOf("banana"))
+
+ 
+// const e = 5;
+// const f = 14;
+// let  a = sum(e,f)
+
+// function sum(a,b){
+//     const sum = (a+b)
+//     return sum
+// }
+
+// function square(a){
+//     const m = a *a;
+//     return m;
+// }
+
+// console.log(square(5))
+
+
+// function greet(){
+//     console.log("Hello World")
+// }
+
+
+
+// const greet = (count) => {
+//     for(let i = 1; i <= count; i++) {
+//         console.log("Hello World");
+//     }
+// }
+// greet(4);
+// */
+// // for(let i=1; i<=10; i++){
+// //     a = Number(5*i)
+// //     b = Number(5*(10-i+1))
+// //     console.log(`5 * ${i} = ${a}        5 * ${10-i+1} = ${b}`)
+// // }
+
+
+// // console.log(i)
+
+
+// // let first = 0
+// // let second = 1
+// // console.log(first)
+// // console.log(second) 
+
+// // for(let i= 1; i<=10; i++){
+// //     sum = Number(first + second)
+// //     console.log(sum)
+// //     first = second
+// //     second = sum
+// // }
+
+
+// // for(let i=1; i<=20; i++){
+// //     if(i==13){
+// //         continue
+// //     }
+// //     console.log(`Floor No. ${i}`)
+
+
+// // }
+
+// // let i = 0length
+
+// // level = prompt("Enter difficulty level: ")
+// // number = Math.floor(Math.random()*100) + 1 
+// // if(level == "Easy"){
+// //     for(let i = 1; i<=20; i++){
+// //         guess = Number(prompt("Guess: "))
+// //         console.log(guess)
+// //         if (guess<number){
+// //             console.log(`Number is higher ${20-i} attempts left`)
+// //         }else{
+// //             console.log(`Number is lower ${20-i} attempts left`)
+// //         }
+// //     }
+// // }
+
+    // emps = [
+    //     {'name': 'Ruchi', 'salary':55000},
+    //     {'name': 'Harshit', 'salary':65000},
+    //     {'name': 'Ram', 'salary':125000},
+    //     {'name': 'Andrew Tate', 'salary':125000}
+
+    // ]
+
+    // sum = 0
+    // for(let i =0; i<emps.length; i++){
+    //     sum = sum + emps[i].salary
+    // }
+    // console.log(sum)
+    // console.log(sum/emps.length)
+    // max_sal = emps[1].salary
+    // min_sal = emps[1].salary
+    // for(let i=0; i<emps.length; i++){
+    //     if (emps[i].salary > max_sal){
+    //         max_sal = emps[i].salary
+    //     }
+    //     if (emps[i].salary < min_sal){
+    //         min_sal = emps[i].salary
+    //     }
+    // }  
+    // for(let i=0; i<emps.length; i++){
+    //     if(emps[i].salary==max_sal){
+    //         console.log(emps[i].name)
+    //     }
+    //     if(emps[i].salary==min_sal){
+    //         console.log(emps[i].name)
+    //     }
+
+    // }
+
+    // function printMsg(name){
+    //     console.log(`Hello ${name}`)
+    // }
+
+    // printMsg("Andrew")
+    // printMsg("Tate")
+
+
+    // function Add(num1, num2){
+    //     sum = Number(num1+num2)
+    //     return sum
+    // }
+
+    // b= Add(5,6)
+    // console.log(b)
+
+    // function Sqr(num1){
+    //     square = Number(num1**2)
+    //     return square
+    // }
+
+    // b= Sqr(5)
+    // console.log(b)
+
+    // function PrintStr(str1, num2){
+    //     for(let i=0; i<num2; i++)
+    //         {
+    //             console.log(str1)
+    //         }
+    // }
+
+    // PrintStr("TOPG",5)
+
+
+
+   
